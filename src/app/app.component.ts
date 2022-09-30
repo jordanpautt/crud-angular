78f97bfa40be32e9c228dfa2a35ff121a1cae675
jordan pautt <pauttjordan7@gmail.com>
2022-09-30 17:12:11 -0500

    add eslint in the project crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud-angular';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('init');
+  }
 }
